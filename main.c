c1f1142e527567bbff4479c2512b65330af0feb6
JohnCamou <94347068+JohnCamou@users.noreply.github.com>
2021-11-19 20:49:15 -0800

    Creating main
    Just basic stuff

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Movie Information!\nYou can use this program to get info on the main actors, gross/net profit, genre and reviews of thousands of movies using the IMDB API");
+
+
+
+
+}
